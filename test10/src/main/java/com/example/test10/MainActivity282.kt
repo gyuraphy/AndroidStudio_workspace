0ff7f050c5a4fdfda49d712f68c1529a01153e7a
gyuraphy <iraem@kakao.com>
2023-03-10 16:42:29 +0900

    230310 7교시 저장!

--- test10/src/main/java/com/example/test10/MainActivity282.kt
+++ test10/src/main/java/com/example/test10/MainActivity282.kt
@@ -5,16 +5,24 @@ import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 import com.example.test10.databinding.ActivityMain282Binding
 
 class MainActivity282 : AppCompatActivity() {
+
+    fun ToastTest(msg:String){
+        Toast.makeText(this, "토스트 샘플 테스트 : ${msg}", Toast.LENGTH_SHORT).show()
+    }
+
     val eventHandler = object : DialogInterface.OnClickListener {
         override fun onClick(p0: DialogInterface?, p1: Int) {
             if (p1 == DialogInterface.BUTTON_POSITIVE) {
-                Log.d("kkang", "positive button click")
+                Log.d("lsy", "positive button click")
+                ToastTest("positive button click")
             } else if (p1 == DialogInterface.BUTTON_NEGATIVE) {
-                Log.d("kkang", "negative button click")
+                Log.d("lsy", "negative button click")
+                ToastTest("negative button click")
             }
         }
     }
@@ -44,8 +52,35 @@ class MainActivity282 : AppCompatActivity() {
                     object : DialogInterface.OnClickListener {
                         override fun onClick(p0: DialogInterface?, p1: Int) {
                             Log.d(
-                                "kkang",
+                                "lsy",
                                 "선택한 과일 : ${items[p1]}}"
+                            )
+                            ToastTest("${items[p1]}")
+                            // 선택한 값에 대한, 데이터 전송이 있음.
+                            // 다른 화면으로의 전환 하는 내용 있음, 인텐트로 전달 할 예정
+                            // 프리퍼런스라는 개념으로 임시 메모리에 저장 후, 가져오기 하는 방식.
+                            // 파이어베이스도 사용할 예졍
+                        }
+                    })
+                setPositiveButton("닫기", null)
+                show()
+            }
+        }
+        // btn3 참고해서 주말 할 일 목록 체크박스 4가지 만들어서
+        // 선택시, 콘솔,토스트 출력하기
+        binding.button5.setOnClickListener {
+            val todos = arrayOf<String>("운동", "영화", "넷플", "산책")
+            AlertDialog.Builder(this).run {
+                setTitle("todos test")
+                setIcon(android.R.drawable.ic_dialog_info)
+                setMultiChoiceItems(
+                    todos,
+                    booleanArrayOf(false, false, false, false),
+                    object : DialogInterface.OnMultiChoiceClickListener {
+                        override fun onClick(p0: DialogInterface?, p1: Int, p2: Boolean) {
+                            Log.d(
+                                "lsy",
+                                "${todos[p1]} 이 ${if (p2) "선택되었습니다." else "선택 해제되었습니다."}"
                             )
                         }
                     })
@@ -53,6 +88,8 @@ class MainActivity282 : AppCompatActivity() {
                 show()
             }
         }
+
+
         binding.button3.setOnClickListener {
             val items = arrayOf<String>("사과", "복숭아", "수박", "딸기")
             AlertDialog.Builder(this).run {
@@ -64,7 +101,7 @@ class MainActivity282 : AppCompatActivity() {
                     object : DialogInterface.OnMultiChoiceClickListener {
                         override fun onClick(p0: DialogInterface?, p1: Int, p2: Boolean) {
                             Log.d(
-                                "kkang",
+                                "lsy",
                                 "${items[p1]} 이 ${if (p2) "선택되었습니다." else "선택 해제되었습니다."}"
                             )
                         }
@@ -84,14 +121,17 @@ class MainActivity282 : AppCompatActivity() {
                     object : DialogInterface.OnClickListener {
                         override fun onClick(p0: DialogInterface?, p1: Int) {
                             Log.d(
-                                "kkang",
+                                "lsy",
                                 "${items[p1]} 이 선택되었습니다.}"
                             )
                         }
                     })
+                // 알림 창 밖에서 뒤로가기 못하게 하기, 닫기 버튼 눌러서 나가기
+                setCancelable(false)
                 setPositiveButton("닫기", null)
                 show()
-            }
+                // 알림 창 밖에서 뒤로가기 못하게 하기, 닫기 버튼 눌러서 나가기
+            }.setCanceledOnTouchOutside(false)
         }
     }
 }
--- test10/src/main/java/com/example/test10/MainActivity288.kt
+++ test10/src/main/java/com/example/test10/MainActivity288.kt
@@ -2,6 +2,8 @@ package com.example.test10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.test10.databinding.ActivityMain288Binding
 import com.example.test10.databinding.DialogInputBinding
@@ -13,10 +15,19 @@ class MainActivity288 : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
+            // DialogInputBinding 사용자가 직접 정의한 xml 파일을 바인딩 했음!
             val dialogBinding = DialogInputBinding.inflate(layoutInflater)
             AlertDialog.Builder(this).run {
                 setTitle("Input")
                 setView(dialogBinding.root)
+                dialogBinding.edit1.setOnClickListener {
+                    Log.d("lsy", "에디터 뷰를 선택 했음")
+                    Toast.makeText(this@MainActivity288, "토스트도 띄웠음", Toast.LENGTH_SHORT).show()
+                }
+                dialogBinding.r1.setOnClickListener {
+                    Log.d("lsy", "r1 뷰를 선택 했음")
+                    Toast.makeText(this@MainActivity288, "r1 토스트도 띄웠음", Toast.LENGTH_SHORT).show()
+                }
                 setPositiveButton("닫기", null)
                 show()
             }
